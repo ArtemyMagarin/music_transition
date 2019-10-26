18fb4aeceda053843229dc319041502c162ffe9a
Artemy Magarin <magar.art@yandex.ru>
2019-10-26 12:51:01 +0300

    fixed bug

--- yandex.py
+++ yandex.py
@@ -24,8 +24,12 @@ class YandexMusicParser(BaseParser):
         owner_id = re.search(r'"uid":"(\d+)"', res, re.IGNORECASE)
         return owner_id.group(1) if owner_id else None
 
-    def fetch_playlists(self, owner_id) -> list:
-        return self.do_post(YandexMusicParser.PLAYLISTS_URL, data={'ids': 1000, 'owner': owner_id}).json()
+    def fetch_playlists(self, username) -> list:
+        res = self.do_get(YandexMusicParser.PLAYLISTS_UI_URL.format(username=username)).text
+        playlist_ids_s = re.search(r'"playlistIds":\[([0-9, ]*?)\]', res, re.IGNORECASE)
+        if playlist_ids_s:
+            return list(map(lambda s: s.strip(), playlist_ids_s.group(1).split(',')))
+        return []
 
     def fetch_playlist(self, owner_id, kind_id):
         res = self.do_get(YandexMusicParser.PLAYLIST_URL.format(owner_id=owner_id, kinds=kind_id)).json()['playlist']
@@ -40,8 +44,8 @@ class YandexMusicParser(BaseParser):
 
     def fetch_data(self, username):
         owner_id = self.fetch_owner_id(username)
-        playlists = self.fetch_playlists(owner_id)
-        return [self.build_playlist(self.fetch_playlist(owner_id, playlist['kind'])) for playlist in playlists]
+        playlist_ids = self.fetch_playlists(username)
+        return [self.build_playlist(self.fetch_playlist(owner_id, playlist)) for playlist in playlist_ids]
 
 
 if __name__ == '__main__':
@@ -49,3 +53,4 @@ if __name__ == '__main__':
     print(ymp.fetch_data('magar.art'))
     print(ymp.fetch_data('v.stromtsova'))
     print(ymp.fetch_data('revyakinpa'))
+    print(ymp.fetch_data('sbazhmin'))
